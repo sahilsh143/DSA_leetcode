dd5818a70d35230c5ad68e93670edfb727207463
sahilsh143 <sahilsharma.cs23@bmsce.ac.in>
2026-07-24 03:59:13 +0530

    Time: 18 ms (15.04%) | Memory: 14.3 MB (33.49%) - LeetSync

---
+++ 1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        int n = s.size();
+        stack<pair<char,int>> st;
+
+        for(int i = 0; i < n; i++){
+            char c = s[i];
+
+            if(st.empty()){
+                st.push({c,1});
+                continue;
+            }
+
+            if(st.top().first != c){
+                st.push({c,1});
+                continue;
+            }
+
+            if(st.top().second < (k-1)){
+                pair<char,int> p = st.top();
+                st.pop();
+                st.push({p.first, p.second + 1});   // count increase
+                continue;
+            }
+
+            st.pop();   // remove when count becomes k
+        }
+
+        string res = "";
+
+        while(!st.empty()){
+            pair<char,int> p = st.top();  // get element
+            st.pop();
+
+            while(p.second--){
+                res.push_back(p.first);
+            }
+        }
+
+        reverse(res.begin(), res.end());  // stack reverses order
+        return res;
+    }
+};
